59c28eaedb2cc3959d459a626b7dee6c0bbc6786
taylor.holmes <taylorh2343@gmail.com>
2022-03-28 00:02:40 -0500

    edited user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,7 +3,7 @@ const { User, Thought } = require('../models');
 const userController = {
     // get all users
     getAllUser(req, res) {
-        user.find({})
+        User.find({})
             .populate({
                 path: 'thoughts',
                 select: '-__v'
